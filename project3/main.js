8033a95ea80fbc7ac3fd275ed78a9d34eaaed3d1
tc4416 <tc4416@nyu.edu>
2026-02-11 15:24:45 -0500

    v2

--- project3/main.js
+++ project3/main.js
@@ -1,4 +1,6 @@
-let q;
+let q = " ";
+let urlQuote = "https://api.kanye.rest";
+let htmlQuote;
 
 window.onload = async () => {
   // checking if the jaba script is succesfully linked and windows loading
@@ -6,46 +8,41 @@ window.onload = async () => {
 
   //changing quote when space pressed / mouse click
   //event listener
-  let quote = document.getElementById("quote");
+  htmlQuote = document.getElementById("quote");
 
-  // checking mouse click
-  document.addEventListener("click", () => {
-    // console.log("click");
-    quote.innerHTML = "mouse is clicked ye";
-  });
-
-  //checking space bar
-  document.addEventListener("keyup", (e) => {
-    if (e.key == " ") {
-      //   console.log("key pressed");
-      //   console.log(e.key);
-      quote.innerHTML = "space bar is pressed ye";
-    }
-  });
-
-  //Geting quate through API
-  let url = "https://api.kanye.rest";
-
-  fetchingQuote(url);
-  //   let responseRaw = await fetch(url);
-  //   console.log(responseRaw);
-
-  //   //translate to json
-  //   let responseQuote = await responseRaw.json();
-  //   console.log(responseQuote);
-  //   console.log(responseQuote.quote);
+  fetchingQuote(urlQuote);
 };
 
 async function fetchingQuote(url) {
   let responseRaw = await fetch(url);
-  console.log(responseRaw);
+  //   console.log(responseRaw);
 
   //translate to json
   let responseQuote = await responseRaw.json();
-  console.log(responseQuote);
+  //  console.log(responseQuote);
 
-  q = responseQuote.quotes;
-  console.log(q);
-
-  return q;
+  q = responseQuote.quote;
+  //   console.log(q);
 }
+
+//moved these outside of windows.onload()
+// checking mouse click
+document.addEventListener("click", () => {
+  // console.log("click");
+  //   quote.innerHTML = "mouse is clicked ye";
+  fetchingQuote(urlQuote);
+  //   console.log(q);
+  htmlQuote.innerHTML = q;
+});
+
+//checking space bar
+document.addEventListener("keyup", (e) => {
+  if (e.key == " ") {
+    //   console.log("key pressed");
+    //   console.log(e.key);
+    // quote.innerHTML = "space bar is pressed ye";
+    fetchingQuote(urlQuote);
+    //   console.log(q);
+    htmlQuote.innerHTML = q;
+  }
+});
